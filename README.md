2f18e94bfb5bab6178c45832c561d15541b2e840
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2024-03-24 15:53:10 +0100

    update

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ## Kafka
 
-Scenario: read 100k msgs and produce them to another topic. Currently only single threaded.
+### Scenario
 
-Result: Go (20s) is dramatically faster than Rust (100s). Still trying to figure out why. Very disappointing.
+Read 100k msgs and produce them to another topic. Commit manually.
+Currently only single threaded.
+
+### Result
+
+Go (20s) is dramatically faster than Rust (100s). Still trying to figure out why.
+
+For Rust we see a dramatic slowdown after the first 10k records (120ms to 500ms-1s) and it never gets faster again.
--- kafka/rust-single/src/main.rs
+++ kafka/rust-single/src/main.rs
@@ -2,14 +2,20 @@ use rdkafka::config::ClientConfig;
 use rdkafka::config::RDKafkaLogLevel;
 use rdkafka::consumer::CommitMode;
 use rdkafka::consumer::{BaseConsumer, Consumer};
+use rdkafka::error::KafkaError;
+use rdkafka::error::RDKafkaErrorCode;
+use rdkafka::message::BorrowedMessage;
 use rdkafka::message::Message;
 use rdkafka::producer::BaseProducer;
 use rdkafka::producer::BaseRecord;
 use rdkafka::producer::Producer;
+use rdkafka::util::Timeout;
+use std::time::Duration;
 use std::time::Instant;
 
 fn main() {
     let producer: BaseProducer = ClientConfig::new()
+        .set("linger.ms", "0")
         .set("bootstrap.servers", "localhost:9092")
         .create()
         .expect("Producer creation failed");
@@ -19,8 +25,7 @@ fn main() {
         .set("group.id", &group_id)
         .set("bootstrap.servers", "localhost:9092")
         .set("enable.auto.commit", "false")
-        .set("auto.offset.reset", "smallest") //mfer this cost me 2 hours because its a default in franz-gp
-        .set("linger.ms", "0")
+        .set("auto.offset.reset", "smallest")
         // .set_log_level(RDKafkaLogLevel::Debug)
         .create()
         .expect("Consumer creation failed");
@@ -32,44 +37,48 @@ fn main() {
     let start = Instant::now();
     println!("start");
 
+    let mut c = Instant::now();
     loop {
-        match consumer.poll(None) {
+        match consumer.poll(Duration::from_secs(1)) {
             None => continue,
             Some(m) => {
                 let m = m.unwrap();
-                producer
-                    .send(
-                        BaseRecord::to("topicB")
-                            .payload(m.payload().unwrap())
-                            .key(m.key().unwrap_or(&[])),
-                    )
-                    .expect("Failed to produce message");
-
-                count += 1;
-
-                if count % 10 == 0 {
-                    producer.poll(None);
+                let payload = m.payload().unwrap();
+                let key = m.key().unwrap_or(&[]);
+                loop {
+                    match producer.send(BaseRecord::to("topicB").payload(payload).key(key)) {
+                        Err((KafkaError::MessageProduction(RDKafkaErrorCode::QueueFull), _)) => {
+                            producer.poll(Duration::from_millis(10));
+                            continue;
+                        }
+                        Err(e) => {
+                            panic!("Error {:?}", e);
+                        }
+                        Ok(_) => break,
+                    }
                 }
-
-                if count % 10000 == 0 {
-                    println!("Processed {}", count);
-                }
-
-                if count == 100_000 {
-                    consumer.commit_message(&m, CommitMode::Async).unwrap();
-                    break;
-                }
-
-                if count % 1000 == 0 {
-                    consumer.commit_message(&m, CommitMode::Async).unwrap();
-                }
+                producer.poll(Duration::from_secs(0));
             }
         };
-    }
-    // Shutdown the consumer and producer
-    consumer.unsubscribe();
-    producer.flush(None).unwrap();
+        count += 1;
 
+        // if count % 10000 == 0 {
+        //     producer.poll(Duration::from_millis(100));
+        // }
+
+        if count % 1000 == 0 {
+            println!("Processed {}, Elapsed {:?}", count, c.elapsed());
+            c = Instant::now();
+        }
+
+        if count == 100_000 {
+            break;
+        }
+    }
+    producer.flush(Timeout::Never).unwrap();
     let elapsed = start.elapsed();
     println!("Time taken: {:?}", elapsed);
+
+    // Shutdown the consumer and producer
+    consumer.unsubscribe();
 }
